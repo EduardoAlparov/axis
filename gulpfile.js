dd0f8dbbe2b56facf508b8c31355a43f5634baaf
Eduard <workman133@yandex.ru>
2021-01-28 18:28:58 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -75,8 +75,7 @@ var gulp = require('gulp'), // подключаем Gulp
 	rigger = require('gulp-rigger'), // модуль для импорта содержимого одного файла в другой
 	runSequence = require('run-sequence'),
 	babel = require('gulp-babel'), //преобразование скриптов с поддержкой ES6
-    removeHtmlComments = require('gulp-remove-html-comments'), //удаление комментариев в html-файлах
-    svgSprite = require('gulp-svg-sprite');
+    removeHtmlComments = require('gulp-remove-html-comments'); //удаление комментариев в html-файлах
 
 
 gulp.task('sass', function (cb) {
@@ -165,22 +164,11 @@ gulp.task('clean', function (cb) {
 	cb();
 });
 
-gulp.task('svgSprite', function () {
-    return gulp.src('app/img/*.svg') // svg files for sprite
-        .pipe(svgSprite({
-                mode: {
-                    stack: {
-                        sprite: "../sprite.svg"  //sprite file name
-                    }
-                },
-            }
-        ))
-        .pipe(gulp.dest(path.dist.img));
-});
+
 
 //gulp.task('default', gulp.series('sass','watch'));
 gulp.task('dev', gulp.series('watch'));
 
-gulp.task('build', gulp.series('clean', 'sass:build', 'useref', 'images', 'fonts', 'script', 'build:delhtmlcomm', 'svgSprite', function (done) {
+gulp.task('build', gulp.series('clean', 'sass:build', 'useref', 'images', 'fonts', 'script', 'build:delhtmlcomm', function (done) {
     done();
 }));
