95105b29eb93cf2ae032173a3955fa2e51d35e9b
Eduard <workman133@yandex.ru>
2021-01-28 22:55:53 +0300

    first commit

--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -1,1 +1,21 @@
-"use strict";document.querySelectorAll(".control-tabs__tab").forEach(function(e){return e.addEventListener("click",function(t){t.preventDefault();t=t.target.getAttribute("href").replace("#","");document.querySelectorAll(".control-tabs__tab").forEach(function(t){return t.classList.remove("control-tabs__tab--active")}),document.querySelectorAll(".section-hero-tabs__block-slide").forEach(function(t){return t.classList.remove("section-hero-tabs__block-slide--active")}),e.classList.add("control-tabs__tab--active"),document.getElementById(t).classList.add("section-hero-tabs__block-slide--active")})}),document.querySelector(".control-tabs__tab").click();
+document.querySelectorAll('.control-tabs__tab').forEach((item) =>
+    item.addEventListener('click', function(e){
+        e.preventDefault();
+
+        const id = e.target.getAttribute('href').replace('#', '');
+
+        document.querySelectorAll('.control-tabs__tab').forEach(
+            (child) => child.classList.remove('control-tabs__tab--active')
+        );
+
+        document.querySelectorAll('.section-hero-tabs__block-slide').forEach(
+            (child) => child.classList.remove('section-hero-tabs__block-slide--active')
+            
+        );
+
+        item.classList.add('control-tabs__tab--active');
+        document.getElementById(id).classList.add('section-hero-tabs__block-slide--active')
+    })
+);
+
+document.querySelector('.control-tabs__tab').click();
--- js/modules/validateForm.js
+++ js/modules/validateForm.js
@@ -1,1 +1,61 @@
-"use strict";function asyncGeneratorStep(e,t,n,r,a,o,i){try{var s=e[o](i),c=s.value}catch(e){return void n(e)}s.done?t(c):Promise.resolve(c).then(r,a)}function _asyncToGenerator(s){return function(){var e=this,i=arguments;return new Promise(function(t,n){var r=s.apply(e,i);function a(e){asyncGeneratorStep(r,t,n,a,o,"next",e)}function o(e){asyncGeneratorStep(r,t,n,a,o,"throw",e)}a(void 0)})}}var mainForm=document.querySelector("#form-contact"),sendBtn=document.querySelector("#send-btn"),statusWindow=document.querySelector(".status"),messages={loading:"Loading...",success:"Thanks! We will contact you.",failure:"Something gone wrong?"};function validateForm(e){var t=!0;return validateField(e.elements.name)||(t=!1),validateField(e.elements.email)||(t=!1),validateField(e.elements.subject)||(t=!1),t=!validateField(e.elements.message)?!1:t}function validateField(e){return e.nextElementSibling.textContent=e.validationMessage,e.checkValidity()}mainForm.addEventListener("submit",function(e){e.preventDefault(),console.log(validateForm(mainForm)),!0===validateForm(mainForm)&&(e=new FormData(mainForm),console.log(e),postData("../server/server.php",e))});var postData=function(){var n=_asyncToGenerator(regeneratorRuntime.mark(function e(t,n){var r;return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return(r=document.createElement("div")).textContent=messages.loading,statusWindow.appendChild(r),e.next=5,fetch(t,{method:"POST",body:n});case 5:return r=e.sent,e.next=8,r.text();case 8:return e.abrupt("return",e.sent);case 9:case"end":return e.stop()}},e)}));return function(e,t){return n.apply(this,arguments)}}();
+const mainForm = document.querySelector('#form-contact');
+const sendBtn = document.querySelector("#send-btn");
+const statusWindow = document.querySelector('.status');
+
+const messages = {
+  loading: 'Loading...',
+  success: 'Thanks! We will contact you.',
+  failure: 'Something gone wrong?'
+};
+
+mainForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log(validateForm(mainForm));
+
+  if(validateForm(mainForm) === true) {
+    const formData = new FormData(mainForm);
+    console.log(formData);
+    postData('../server/server.php', formData)
+  }
+
+});
+
+function validateForm(form) {
+  let valid = true;
+
+  if(!validateField(form.elements.name)) {
+      valid = false;
+  }
+
+  if(!validateField(form.elements.email)) {
+      valid = false;
+  }
+
+  if(!validateField(form.elements.subject)) {
+      valid = false;
+  }
+
+  if(!validateField(form.elements.message)) {
+      valid = false;
+  }
+
+  return valid;
+}
+
+function validateField(field) {
+  field.nextElementSibling.textContent = field.validationMessage;
+  return field.checkValidity();
+}
+
+const postData = async (url, data) => {
+  const div = document.createElement('div');
+  div.textContent = messages.loading;
+  statusWindow.appendChild(div);
+  let res = await fetch(url, {
+    method: "POST",
+    body: data
+  });
+
+  return await res.text();
+};
