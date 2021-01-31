8089016d528cdc2c8e656b62f4ef3e2be990d1a7
Eduard <workman133@yandex.ru>
2021-01-31 22:43:10 +0300

    final commit

--- app/js/start.js
+++ app/js/start.js
@@ -1,4 +1,4 @@
-import { hamburger } from './modules/hamburger.js';
+import './modules/hamburger.js';
 import './modules/inputFileLoader.js';
 import './modules/menuNavigate.js';
 import './modules/sliderReviews.js';
--- dist/js/start.js
+++ dist/js/start.js
@@ -1,1 +1,1 @@
-"use strict";var _hamburger=require("./modules/hamburger.js");require("./modules/inputFileLoader.js"),require("./modules/menuNavigate.js"),require("./modules/sliderReviews.js"),require("./modules/tabs.js"),require("./modules/validateForm.js");
+"use strict";require("./modules/hamburger.js"),require("./modules/inputFileLoader.js"),require("./modules/menuNavigate.js"),require("./modules/sliderReviews.js"),require("./modules/tabs.js"),require("./modules/validateForm.js");
